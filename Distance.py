2af2469b6a0e26225865f83e3defa8ebe642e9f1
ahmad <abomoaz3375@gmail.com>
2022-03-11 21:38:39 +0300

    Initial commit

---
+++ Distance.py
@@ -0,0 +1,21 @@
+from math import radians, cos, sin, asin, sqrt
+
+
+# lon = خط الطول
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    # Radius of earth in kilometers is 6371
+    km = 6371 * c
+    return km
+
+# print(haversine(46.738586,24.774265,39.8579,21.3891))
